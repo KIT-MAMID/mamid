7ad8fb2875e24861be450a26c48de58e2f8e29ec
Anton Schirg <anton.schirg@posteo.de>
2016-06-28 19:11:24 +0200

    ADD: Bus test

---
+++ mamidmaster/bus.go
@@ -0,0 +1,34 @@
+package mamidmaster
+
+import (
+	"reflect"
+)
+
+type Bus struct {
+	channels []chan interface{}
+}
+
+func (b *Bus) GetNewChannel() chan interface{} {
+	channel := make(chan interface{})
+	b.channels = append(b.channels, channel)
+	//TODO Interrupt Bus.Run Select when adding a new channel
+	return channel
+}
+
+func (b *Bus) Run() {
+	for {
+		selects := make([]reflect.SelectCase, len(b.channels))
+		for i, channel := range b.channels {
+			selects[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(channel)}
+		}
+
+		chosen, recv, recvOk := reflect.Select(selects)
+		if recvOk {
+			for i, channel := range b.channels {
+				if i != chosen {
+					channel<-recv
+				}
+			}
+		}
+	}
+}
