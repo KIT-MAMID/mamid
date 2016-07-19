1186bd056d7b77735af2dbc10f6f07f97a0479a6
Anton Schirg <anton.schirg@posteo.de>
2016-07-19 16:22:35 +0200

    ADD: GUI: Show replica sets

--- gui/static/js/script.js
+++ gui/static/js/script.js
@@ -14,10 +14,21 @@ mamidApp.config(function ($routeProvider) {
            templateUrl: 'pages/slave.html',
            controller: 'slaveByIdController'
        })
+       .when('/replicasets', {
+           templateUrl: 'pages/replicasets.html',
+           controller: 'replicasetIndexController'
+       })
 });
 
 mamidApp.factory('SlaveService', function ($resource) {
     return $resource('/api/slaves/:slave', {slave: "@id"}, {
+        create: {method: 'put'},
+        queryByReplicaSet: {method: 'get', url: '/api/replicasets/:replicaset/slaves/'}
+    });
+});
+
+mamidApp.factory('ReplicaSetService', function ($resource) {
+    return $resource('/api/replicasets/:replicaset', {replicaset: "@id"}, {
         create: {method: 'put'}
     });
 });
@@ -42,7 +53,7 @@ mamidApp.controller('slaveByIdController', function($scope, $http, $routeParams,
         $scope.edit_slave = angular.copy($scope.slave);
     } else {
         $scope.slave = SlaveService.get({slave: slaveId});
-        
+
         //Copy slave for edit form so that changes are only applied to model when apply is clicked
         $scope.slave.$promise.then(function(){
             $scope.edit_slave = angular.copy($scope.slave);
@@ -69,4 +80,8 @@ mamidApp.controller('slaveByIdController', function($scope, $http, $routeParams,
         $scope.slave.state = state;
         $scope.slave.$save();
     }
+});
+
+mamidApp.controller('replicasetIndexController', function($scope, $http, ReplicaSetService) {
+    $scope.replicasets = ReplicaSetService.query()
 });
