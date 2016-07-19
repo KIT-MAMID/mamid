daa39dd96b1bf71d2cb721a11b202b29b52e5e57
Anton Schirg <anton.schirg@posteo.de>
2016-07-19 19:36:44 +0200

    FIX: Formatting and typos

--- master/masterapi/masterapi_test.go
+++ master/masterapi/masterapi_test.go
@@ -1,16 +1,16 @@
 package masterapi
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"net/http/httptest"
-	"github.com/KIT-MAMID/mamid/model"
-	"net/http"
-	"github.com/gorilla/mux"
-	"github.com/KIT-MAMID/mamid/master"
 	"encoding/json"
+	"github.com/KIT-MAMID/mamid/master"
+	"github.com/KIT-MAMID/mamid/model"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"strings"
+	"testing"
 )
 
 func createDBAndMasterAPI() (db *gorm.DB, mainRouter *mux.Router, err error) {
@@ -23,7 +23,7 @@ func createDBAndMasterAPI() (db *gorm.DB, mainRouter *mux.Router, err error) {
 		MongodPortRangeEnd:   3,
 		PersistentStorage:    true,
 		Mongods:              []*model.Mongod{},
-		ConfiguedState:       model.SlaveStateActive,
+		ConfiguredState:      model.SlaveStateActive,
 	}
 	db.Create(&dbSlave)
 
@@ -34,10 +34,9 @@ func createDBAndMasterAPI() (db *gorm.DB, mainRouter *mux.Router, err error) {
 		MongodPortRangeEnd:   200,
 		PersistentStorage:    false,
 		Mongods:              []*model.Mongod{},
-		ConfiguedState:       model.SlaveStateDisabled,
+		ConfiguredState:      model.SlaveStateDisabled,
 	}
 	db.Create(&dbSlave2)
-
 
 	// Setup masterapi
 	clusterAllocator := &master.ClusterAllocator{}
@@ -54,7 +53,7 @@ func createDBAndMasterAPI() (db *gorm.DB, mainRouter *mux.Router, err error) {
 }
 
 func TestMasterAPI_SlaveIndex(t *testing.T) {
-	_, mainRouter, err := createDBAndMasterAPI();
+	_, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	// Test correct get
@@ -73,12 +72,12 @@ func TestMasterAPI_SlaveIndex(t *testing.T) {
 	assert.Equal(t, 1, getSlaveResult[0].Port)
 	assert.Equal(t, 2, getSlaveResult[0].MongodPortRangeBegin)
 	assert.Equal(t, 3, getSlaveResult[0].MongodPortRangeEnd)
-	assert.Equal(t, true, getSlaveResult[0].PersistantStorage)
-	assert.Equal(t, "active", getSlaveResult[0].State)
+	assert.Equal(t, true, getSlaveResult[0].PersistentStorage)
+	assert.Equal(t, "active", getSlaveResult[0].ConfiguredState)
 }
 
 func TestMasterAPI_SlaveById(t *testing.T) {
-	_, mainRouter, err := createDBAndMasterAPI();
+	_, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	// Test correct get
@@ -96,18 +95,18 @@ func TestMasterAPI_SlaveById(t *testing.T) {
 	assert.Equal(t, 1, getSlaveResult.Port)
 	assert.Equal(t, 2, getSlaveResult.MongodPortRangeBegin)
 	assert.Equal(t, 3, getSlaveResult.MongodPortRangeEnd)
-	assert.Equal(t, true, getSlaveResult.PersistantStorage)
-	assert.Equal(t, "active", getSlaveResult.State)
+	assert.Equal(t, true, getSlaveResult.PersistentStorage)
+	assert.Equal(t, "active", getSlaveResult.ConfiguredState)
 }
 
 func TestMasterAPI_SlavePut(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test correct put
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id\":0,\"hostname\":\"createdhost\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"desired_state\":\"disabled\"}"
+	req_body := "{\"id\":0,\"hostname\":\"createdhost\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"configured_state\":\"disabled\"}"
 	req, err := http.NewRequest("PUT", "/api/slaves", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -123,16 +122,16 @@ func TestMasterAPI_SlavePut(t *testing.T) {
 	assert.Equal(t, 20000, createdSlave.MongodPortRangeBegin)
 	assert.Equal(t, 20001, createdSlave.MongodPortRangeEnd)
 	assert.Equal(t, false, createdSlave.PersistentStorage)
-	assert.Equal(t, model.SlaveStateDisabled, createdSlave.ConfiguedState)
+	assert.Equal(t, model.SlaveStateDisabled, createdSlave.ConfiguredState)
 }
 
 func TestMasterAPI_SlavePut_invalid(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 	//Test invalid put (non existing field)
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id_invalid_blabla\":0,\"hostname\":\"createdhost_invalid\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"desired_state\":\"disabled\"}"
+	req_body := "{\"id_invalid_blabla\":0,\"hostname\":\"createdhost_invalid\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"configured_state\":\"disabled\"}"
 	req, err := http.NewRequest("PUT", "/api/slaves", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -146,13 +145,13 @@ func TestMasterAPI_SlavePut_invalid(t *testing.T) {
 }
 
 func TestMasterAPI_SlaveUpdate(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test valid update
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id\":2,\"hostname\":\"updHost\",\"slave_port\":2,\"mongod_port_range_begin\":101,\"mongod_port_range_end\":201,\"persistant_storage\":true,\"desired_state\":\"disabled\"}"
+	req_body := "{\"id\":2,\"hostname\":\"updHost\",\"slave_port\":2,\"mongod_port_range_begin\":101,\"mongod_port_range_end\":201,\"persistant_storage\":true,\"configured_state\":\"disabled\"}"
 	req, err := http.NewRequest("POST", "/api/slaves/2", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -167,17 +166,17 @@ func TestMasterAPI_SlaveUpdate(t *testing.T) {
 	assert.Equal(t, 101, updatedSlave.MongodPortRangeBegin)
 	assert.Equal(t, 201, updatedSlave.MongodPortRangeEnd)
 	assert.Equal(t, true, updatedSlave.PersistentStorage)
-	assert.Equal(t, model.SlaveStateDisabled, updatedSlave.ConfiguedState)
+	assert.Equal(t, model.SlaveStateDisabled, updatedSlave.ConfiguredState)
 }
 
 func TestMasterAPI_SlaveUpdate_invalid(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test invalid update (slave is in active state)
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id\":1,\"hostname\":\"updHost\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"desired_state\":\"active\"}"
+	req_body := "{\"id\":1,\"hostname\":\"updHost\",\"slave_port\":1912,\"mongod_port_range_begin\":20000,\"mongod_port_range_end\":20001,\"persistant_storage\":false,\"configured_state\":\"active\"}"
 	req, err := http.NewRequest("POST", "/api/slaves/1", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -192,17 +191,17 @@ func TestMasterAPI_SlaveUpdate_invalid(t *testing.T) {
 	assert.Equal(t, 2, updatedSlave.MongodPortRangeBegin)
 	assert.Equal(t, 3, updatedSlave.MongodPortRangeEnd)
 	assert.Equal(t, true, updatedSlave.PersistentStorage)
-	assert.Equal(t, model.SlaveStateActive, updatedSlave.ConfiguedState)
+	assert.Equal(t, model.SlaveStateActive, updatedSlave.ConfiguredState)
 }
 
 func TestMasterAPI_SlaveUpdate_change_desired_state(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test valid state change
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id\":2,\"hostname\":\"host2\",\"slave_port\":1,\"mongod_port_range_begin\":100,\"mongod_port_range_end\":200,\"persistant_storage\":false,\"desired_state\":\"active\"}"
+	req_body := "{\"id\":2,\"hostname\":\"host2\",\"slave_port\":1,\"mongod_port_range_begin\":100,\"mongod_port_range_end\":200,\"persistant_storage\":false,\"configured_state\":\"active\"}"
 	req, err := http.NewRequest("POST", "/api/slaves/2", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -212,17 +211,17 @@ func TestMasterAPI_SlaveUpdate_change_desired_state(t *testing.T) {
 	var updatedSlave model.Slave
 	db.First(&updatedSlave, 2)
 
-	assert.Equal(t, model.SlaveStateActive, updatedSlave.ConfiguedState)
+	assert.Equal(t, model.SlaveStateActive, updatedSlave.ConfiguredState)
 }
 
 func TestMasterAPI_SlaveUpdate_change_desired_state_invalid(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test invalid state change (should not be able to change state while changing another parameter)
 	resp := httptest.NewRecorder()
 
-	req_body := "{\"id\":2,\"hostname\":\"host2\",\"slave_port\":1,\"mongod_port_range_begin\":100,\"mongod_port_range_end\":150,\"persistant_storage\":false,\"desired_state\":\"active\"}"
+	req_body := "{\"id\":2,\"hostname\":\"host2\",\"slave_port\":1,\"mongod_port_range_begin\":100,\"mongod_port_range_end\":150,\"persistant_storage\":false,\"configured_state\":\"active\"}"
 	req, err := http.NewRequest("POST", "/api/slaves/2", strings.NewReader(req_body))
 	assert.NoError(t, err)
 	mainRouter.ServeHTTP(resp, req)
@@ -232,11 +231,11 @@ func TestMasterAPI_SlaveUpdate_change_desired_state_invalid(t *testing.T) {
 	var updatedSlave model.Slave
 	db.First(&updatedSlave, 2)
 
-	assert.Equal(t, model.SlaveStateDisabled, updatedSlave.ConfiguedState)
+	assert.Equal(t, model.SlaveStateDisabled, updatedSlave.ConfiguredState)
 }
 
 func TestMasterAPI_SlaveDelete(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test valid delete
@@ -255,7 +254,7 @@ func TestMasterAPI_SlaveDelete(t *testing.T) {
 }
 
 func TestMasterAPI_SlaveDelete_invalid(t *testing.T) {
-	db, mainRouter, err := createDBAndMasterAPI();
+	db, mainRouter, err := createDBAndMasterAPI()
 	assert.NoError(t, err)
 
 	//Test invalid delete (active slave)
