887c9a191bf3723a09ed51a296dd6a660e163ba3
Niklas Fuhrberg <niklas.fuhrberg@gmx.de>
2016-07-27 13:00:42 +0200

    ADD: Email Notifier - sending mail

--- notifier/main.go
+++ notifier/main.go
@@ -3,12 +3,45 @@ package main
 import (
 	"os"
 	"os/signal"
+	"fmt"
 )
+var email EmailNotifier
+var lastProblems []Problem
+//var notifiers []Notifier
+type Problem struct{
+
+}
 
 func main() {
 	// Wait forever
+	//Test
+	var problem Problem
+	notify(problem)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
+	var currentProblems []Problem
+        currentProblems = diffProblems(currentProblems)
+        for i := 0; i < len(currentProblems); i++{
+                notify(currentProblems[i])
+        }
 	<-c
+	fmt.Println("A1");
 	os.Exit(0)
+	fmt.Println("B");
+	//receive Problems through API
 }
+func diffProblems(received []Problem) []Problem {
+	for i := 0; i < len(received); i++ {
+		for j := 0; j < len(lastProblems); j++{
+			if(true){
+				received = append(received[:i], received[i+1:]...)
+			}
+		}
+	}
+	return received
+}
+
+func notify(problem Problem){
+	email.SendProblem(problem)
+}
+
--- notifier/notifiers.go
+++ notifier/notifiers.go
@@ -1,17 +1,41 @@
 package main
 
+import(
+	"net/smtp"
+	"fmt"
+//	"bytes"
+	"log"
+	"strings"
+)
 type Notifier interface {
 	SendProblem(problem Problem) error
 }
 
+type EmailContact struct {
+	Address string
+}
 type EmailNotifier struct {
 	Contacts []*EmailContact
 }
-
-func (n *EmailNotifier) SendProblem(problem *Problem) error {
+func (n *EmailNotifier) SendProblem(problem Problem) error {
+	msg := []byte("To: niklas.fuhrberg@gmx.de\r\n"+
+                        "From: kit.mamid@gmail.com\r\n"+
+                        "Subject: TESST123\r\n")
+	fmt.Println("msg")
+	n.sendMailToContacts(msg)
+	return nil
+}
+func (n *EmailNotifier) sendMailToContacts(msg []byte) error{
+	var auth = smtp.PlainAuth("", "kit.mamid@gmail.com", "uwsngsdlsnh", "smtp.gmail.com")
+	var err = smtp.SendMail(
+		"smtp.gmail.com:465",
+		auth,
+		"kit.mamid@gmail.com",
+		[]string{"niklas.fuhrberg@gmx.de"},
+		msg)
+	if err != nil{
+		log.Printf("sendSmtp: failure: %q", strings.Split(err.Error(), "\n"))
+	}
 	return nil
 }
 
-func (n *EmailNotifier) sendMailToContacts(msg string) error {
-	return nil
-}
