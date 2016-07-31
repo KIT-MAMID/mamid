958a98a357a5addbe3d777971d36edaa085914f2
Christian Schwarz <me@cschwarz.com>
2016-07-31 21:36:47 +0200

    pq: convert prototypes to custom structures.

--- master/cluster_allocator_pq.go
+++ master/cluster_allocator_pq.go
@@ -1,13 +1,36 @@
 package master
 
 import (
+	. "github.com/KIT-MAMID/mamid/model"
 	"github.com/jinzhu/gorm"
 )
 
-func (c *ClusterAllocator) pqRiskGroups(tx *gorm.DB) map[interface{}]interface{} {
-	return make(map[interface{}]interface{})
+type pqSlavesByRiskGroup struct {
 }
 
-func (c *ClusterAllocator) pqReplicaSets(tx *gorm.DB) interface{} {
-	return nil
+func (q *pqSlavesByRiskGroup) pushSlave(s Slave) {
+	// assuming RiskGroupID is set
+}
+
+func (q *pqSlavesByRiskGroup) popSlaveinNonconflictingRiskGroup(r RiskGroup) {
+
+}
+
+func (c *ClusterAllocator) pqRiskGroups(tx *gorm.DB) *pqSlavesByRiskGroup {
+	return &pqSlavesByRiskGroup{}
+}
+
+type pqReplicaSets struct {
+}
+
+func (q *pqReplicaSets) Push(r *ReplicaSet) {
+
+}
+
+func (q *pqReplicaSets) Pop(r *ReplicaSet) {
+
+}
+
+func (c *ClusterAllocator) pqReplicaSets(tx *gorm.DB) *pqReplicaSets {
+	return &pqReplicaSets{}
 }
