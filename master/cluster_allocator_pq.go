1b79826d51242471ff3f5db20315eb1f9b416daa
Christian Schwarz <me@cschwarz.com>
2016-08-05 22:27:41 +0200

    ADD: Mongod PQ implementation.

---
+++ master/cluster_allocator_pq.go
@@ -0,0 +1,28 @@
+package master
+
+type pqSlice struct {
+	Slice          []interface{}
+	LessComparator func(i, j interface{}) bool
+}
+
+func (s pqSlice) Len() int {
+	return len(s.Slice)
+}
+
+func (s pqSlice) Less(left, right int) bool {
+	return s.LessComparator(&s.Slice[left], &s.Slice[right])
+}
+
+func (s *pqSlice) Swap(i, j int) {
+	s.Slice[i], s.Slice[j] = s.Slice[j], s.Slice[i]
+}
+
+func (s pqSlice) Push(i interface{}) {
+	s.Slice = append(s.Slice, i)
+}
+
+func (s pqSlice) Pop() interface{} {
+	ret := s.Slice[len(s.Slice)-1]
+	s.Slice = s.Slice[0 : len(s.Slice)-1]
+	return ret
+}
--- master/cluster_allocator_pq_mongods.go
+++ master/cluster_allocator_pq_mongods.go
@@ -1,16 +1,62 @@
 package master
 
 import (
+	"container/heap"
 	. "github.com/KIT-MAMID/mamid/model"
 )
 
 type pqMongods struct {
+	slice pqSlice
+}
+
+type pqMongodItem struct {
+	Mongod         *Mongod
+	ParentBusyRate float64
+}
+
+func pqMongodItemFromMongod(mongod *Mongod) *pqMongodItem {
+
+	maxMongods := float64(slaveMaxNumberOfMongods(mongod.ParentSlave))
+	runningMongods := float64(len(mongod.ParentSlave.Mongods))
+
+	return &pqMongodItem{
+		Mongod:         mongod,
+		ParentBusyRate: runningMongods / maxMongods,
+	}
 }
 
 func (q *pqMongods) PopMongodOnBusiestSlave() *Mongod {
-	return nil
+	item, ok := heap.Pop(&q.slice).(*pqMongodItem)
+	if !ok {
+		panic("unexpected type in pqMongods")
+	}
+	return item.Mongod
 }
 
 func (c *ClusterAllocator) pqMongods(mongods []*Mongod, p persistence) *pqMongods {
-	return &pqMongods{}
+
+	items := make([]interface{}, len(mongods))
+	for _, m := range mongods {
+		if slavePersistence(m.ParentSlave) == p {
+			items = append(items, pqMongodItemFromMongod(m))
+		}
+	}
+
+	q := &pqMongods{
+		slice: pqSlice{
+			items,
+			func(i, j interface{}) bool {
+				a, a_ok := i.(*pqMongodItem)
+				b, b_ok := j.(*pqMongodItem)
+				if !a_ok || !b_ok {
+					panic("unexpected type in pqMongods")
+				}
+				return a.ParentBusyRate > b.ParentBusyRate //'>' to have busiest node at top of heap TODO correct
+			},
+		},
+	}
+
+	heap.Init(&q.slice)
+
+	return q
 }
