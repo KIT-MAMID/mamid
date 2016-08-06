e1685b20b84fb2cd942dea3c0b2580fac6943776
Christian Schwarz <me@cschwarz.com>
2016-08-07 00:32:36 +0200

    pqReplicaSets: switch to pqSlice

--- master/cluster_allocator_pq_replica_sets.go
+++ master/cluster_allocator_pq_replica_sets.go
@@ -5,21 +5,27 @@ import (
 	. "github.com/KIT-MAMID/mamid/model"
 )
 
-////////////////////////////////////////////////////////////////////////////////
-// Public Interface
-// TODO use pqSlice instead of pqReplicaSetItemSlice
-////////////////////////////////////////////////////////////////////////////////
-
 type pqReplicaSets struct {
-	slice pqReplicaSetItemSlice
+	slice pqSlice
+	p     persistence
+}
+
+type pqReplicaSetItem struct {
+	r               *ReplicaSet
+	p               persistence
+	relMemberCounts map[persistence]float64
+	degraded        map[persistence]bool
 }
 
 func (c *ClusterAllocator) pqReplicaSets(replicaSets []*ReplicaSet, p persistence) *pqReplicaSets {
 
 	pq := &pqReplicaSets{
-		slice: pqReplicaSetItemSlice{
-			items: make([]*pqReplicaSetItem, 0),
-			p:     p,
+		p: p,
+		slice: pqSlice{
+			Slice: make([]interface{}, 0),
+			LessComparator: func(i, j interface{}) bool {
+				return i.(*pqReplicaSetItem).relMemberCounts[p] < j.(*pqReplicaSetItem).relMemberCounts[p]
+			},
 		},
 	}
 
@@ -33,62 +39,6 @@ func (c *ClusterAllocator) pqReplicaSets(replicaSets []*ReplicaSet, p persistenc
 	heap.Init(&pq.slice)
 
 	return pq
-}
-
-func (q *pqReplicaSets) Pop() *ReplicaSet {
-	if q.slice.Len() <= 0 {
-		return nil
-	}
-	return heap.Pop(&q.slice).(*pqReplicaSetItem).r
-}
-
-func (q *pqReplicaSets) PushIfDegraded(r *ReplicaSet) {
-	item := replicaSetItemFromReplicaSet(r)
-	if item.degraded[q.slice.p] {
-		heap.Push(&q.slice, item)
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Hidden Implementation using container/heap
-////////////////////////////////////////////////////////////////////////////////
-
-type pqReplicaSetItemSlice struct {
-	items []*pqReplicaSetItem
-	p     persistence
-}
-
-type pqReplicaSetItem struct {
-	r               *ReplicaSet
-	p               persistence
-	relMemberCounts map[persistence]float64
-	degraded        map[persistence]bool
-}
-
-func (s pqReplicaSetItemSlice) Len() int {
-	return len(s.items)
-}
-
-func (s pqReplicaSetItemSlice) Less(left, right int) bool {
-	return s.items[left].relMemberCounts[s.p] < s.items[right].relMemberCounts[s.p]
-}
-
-func (s *pqReplicaSetItemSlice) Swap(i, j int) {
-	s.items[i], s.items[j] = s.items[j], s.items[i]
-}
-
-func (s *pqReplicaSetItemSlice) Push(i interface{}) {
-	item, ok := i.(*pqReplicaSetItem)
-	if !ok {
-		panic("pqReplicaSetItemSlice should only be used with *ReplicaSet")
-	}
-	s.items = append(s.items, item)
-}
-
-func (s *pqReplicaSetItemSlice) Pop() interface{} {
-	ret := s.items[len(s.items)-1]
-	s.items = s.items[0 : len(s.items)-1]
-	return ret
 }
 
 func replicaSetItemFromReplicaSet(r *ReplicaSet) *pqReplicaSetItem {
@@ -132,5 +82,19 @@ func replicaSetItemFromReplicaSet(r *ReplicaSet) *pqReplicaSetItem {
 		r:               r,
 		relMemberCounts: relMemberCounts,
 		degraded:        degraded,
+	}
+}
+
+func (q *pqReplicaSets) Pop() *ReplicaSet {
+	if q.slice.Len() <= 0 {
+		return nil
+	}
+	return heap.Pop(&q.slice).(*pqReplicaSetItem).r
+}
+
+func (q *pqReplicaSets) PushIfDegraded(r *ReplicaSet) {
+	item := replicaSetItemFromReplicaSet(r)
+	if item.degraded[q.p] {
+		heap.Push(&q.slice, item)
 	}
 }
