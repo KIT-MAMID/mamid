4a52279f419188fd7288f2ddb961f489ee2bde19
Janis Streib <janis@dogcraft.de>
2016-08-09 23:37:37 +0200

    UPD: use SMTPRelay

--- notifier/notifiers.go
+++ notifier/notifiers.go
@@ -20,22 +20,21 @@ func (n *EmailNotifier) SendProblem(problem Problem) error {
 		"Slave: " + fmt.Sprint(problem.Slave) + "\r\n" +
 		"long Description:" + problem.LongDescription) + "\r\n"
 	subject := ("Subject:" + "KIT-MAMID: Problem in " + fmt.Sprint(problem.ReplicaSet) + "/" + fmt.Sprint(problem.Slave))
-	msg := []byte("From: kit.mamid@gmail.com\r\n" +
+	msg := []byte("From: " + n.Relay.MailFrom + "\r\n" +
 		subject + "\r\n" +
 		content)
 	return n.sendMailToContacts(msg)
 }
 
 func (n *EmailNotifier) sendMailToContacts(msg []byte) error {
-	auth := smtp.PlainAuth("", "kit.mamid@gmail.com", "uwsngsdlsnh", "smtp.gmail.com")
 	var to []string
 	for i := 0; i < len(n.Contacts); i++ {
 		to = append(to, n.Contacts[i].Address)
 	}
 	err := smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		"kit.mamid@gmail.com",
+		n.Relay.Hostname,
+		nil,
+		n.Relay.MailFrom,
 		to,
 		msg)
 	return err
