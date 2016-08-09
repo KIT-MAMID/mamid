bd20ade02742bc0c7463a9cb71654677dd388334
Anton Schirg <anton.schirg@posteo.de>
2016-08-09 21:35:52 +0200

    ADD: problem manager: handle DesiredReplicaSetConstraintStatus

--- master/problem_manager.go
+++ master/problem_manager.go
@@ -35,6 +35,31 @@ func (p *ProblemManager) Run() {
 					SlaveID:     connStatus.Slave.ID,
 				}).Delete(&model.Problem{})
 			}
+		case model.DesiredReplicaSetConstraintStatus:
+			constrStatus := message.(model.DesiredReplicaSetConstraintStatus)
+			if constrStatus.Unsatisfied {
+				var problem model.Problem
+				p.DB.Where(&model.Problem{
+					ProblemType:  model.ProblemTypeDesiredReplicaSetConstraint,
+					ReplicaSetID: constrStatus.ReplicaSet.ID,
+				}).Assign(&model.Problem{
+					Description: fmt.Sprintf("Replica set %s is degraded", constrStatus.ReplicaSet.Name),
+					LongDescription: fmt.Sprintf(
+						"Not enough free ports are available."+
+							"This replica set is now configured to have %d persistent and %d volatile mongods"+
+							" instead of the %d persistent and %d volatile mongods it should have",
+						constrStatus.ActualPersistentCount, constrStatus.ActualVolatileCount,
+						constrStatus.ReplicaSet.PersistentMemberCount, constrStatus.ReplicaSet.VolatileMemberCount),
+					LastUpdated: time.Now(),
+				}).Attrs(&model.Problem{
+					FirstOccurred: time.Now(),
+				}).FirstOrCreate(&problem)
+			} else {
+				p.DB.Where(&model.Problem{
+					ProblemType:  model.ProblemTypeDesiredReplicaSetConstraint,
+					ReplicaSetID: constrStatus.ReplicaSet.ID,
+				}).Delete(&model.Problem{})
+			}
 		}
 	}
 
