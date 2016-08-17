477fe6a0353d13f5fb9044f6f2e8e75d58554456
Christian Schwarz <me@cschwarz.com>
2016-08-17 20:23:10 +0200

    UPD: slave: parse shutdown times as time.Duration in main()

--- slave/cmd/slave.go
+++ slave/cmd/slave.go
@@ -10,18 +10,28 @@ import (
 )
 
 const MongodExecutableDefaultName = "mongod"
+const DefaultMongodSoftShutdownTimeout = "3s" // seconds
+const DefaultMongodHardShutdownTimeout = "5s" // seconds
 
 func main() {
 
 	var (
-		mongodExecutable, dataDir string
+		mongodExecutable, dataDir                                  string
+		mongodSoftShutdownTimeoutStr, mongodHardShutdownTimeoutStr string
 	)
 
 	flag.StringVar(&dataDir, "data", "", "Persistent data and slave configuration directory")
 	mongodExecutableLookupPath, _ := exec.LookPath(MongodExecutableDefaultName)
 	flag.StringVar(&mongodExecutable, "mongodExecutable", mongodExecutableLookupPath, "Path to or name of Mongod binary")
 
+	flag.StringVar(&mongodSoftShutdownSeconds, "mongod.shutdownTimeout.soft", DefaultMongodSoftShutdownTimeout,
+		"Duration to wait for regular Mongod shutdown call to return. Specify with suffix [ms,s,min,...]")
+	flag.StringVar(&mongodHardShutdownSeconds, "mongod.shutdownTimeout.hard", DefaultMongodHardShutdownTimeout,
+		"Duration to wait after issuing a shutdown call before the Mongod is killed (SIGKILL). Specify with suffix [ms,s,min,...]")
+
 	flag.Parse()
+
+	// Assert dataDir is valid. TODO should we do this lazyly?
 
 	if dataDir == "" {
 		println("No root data directory passed; specify with -data=/path/to/root/dir")
@@ -40,7 +50,20 @@ func main() {
 		}
 	}
 
-	controller := NewController(mongodExecutable, dataDir)
+	// Convert timeouts to internal representation
+
+	mongodSoftShutdownTimeout, err := time.ParseDuration(mongodSoftShutdownTimeoutStr)
+	if !err {
+		fmt.Printf("could not convert soft shutdown timeout to time.Duration: %s", err)
+		return
+	}
+
+	mongodHardShutdownTimeout, err := time.Duration(mongodHardShutdownTimeoutStr)
+	if !err {
+		fmt.Printf("could not convert hard shutdown timeout to time.Duration: %s", err)
+		return
+	}
+
 	server := msp.NewServer(controller)
 	server.Run()
 }
