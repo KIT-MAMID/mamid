e70c1deb59f75236fbbf9547465888cf9e35f54e
Christian Schwarz <me@cschwarz.com>
2016-08-17 18:48:37 +0200

    SQUASHTHIS Add remarks in form of TODO comments.

--- slave/controller.go
+++ slave/controller.go
@@ -8,13 +8,14 @@ import (
 	"time"
 )
 
+// TODO make all these constants defaults for CLI parameters.
 const DataDBDir = "db"
 
 const MongodSoftShutdownTimeout = 3
 const MongodHardShutdownTimeout = 5
 
 type Controller struct {
-	processes    ProcessManager
+	processes    ProcessManager // TODO rename variable
 	configurator MongodConfigurator
 
 	busyTable     map[msp.PortNumber]*sync.Mutex
@@ -23,7 +24,7 @@ type Controller struct {
 
 func NewController(dataDir string) *Controller {
 	return &Controller{
-		processes:     NewProcessManager("mongod", dataDir),
+		processes:     NewProcessManager("mongod", dataDir), // TODO make the binary path a cli parameter, defaulting to "mongod" from PATH environment variable (using os.Env)
 		configurator:  &ConcreteMongodConfigurator{mgo.Dial},
 		busyTable:     make(map[msp.PortNumber]*sync.Mutex),
 		busyTableLock: sync.Mutex{},
@@ -35,7 +36,7 @@ func (c *Controller) RequestStatus() ([]msp.Mongod, *msp.Error) {
 	mongods := make([]msp.Mongod, len(ports))
 	for k, port := range ports {
 		var err *msp.Error
-		mongods[k], err = c.configurator.MongodConfiguration(port)
+		mongods[k], err = c.configurator.MongodConfiguration(port) // TODO parallelize this? => use goroutines that send into a channel whic appends (single-threaded) to the array
 		if err != nil {
 			return nil, err
 		}
@@ -56,9 +57,9 @@ func (c *Controller) EstablishMongodState(m msp.Mongod) *msp.Error {
 
 		if err != nil {
 			return &msp.Error{
-				Identifier:      fmt.Sprintf("spawn_%d", m.Port),
+				Identifier:      fmt.Sprintf("spawn_%d", m.Port), // TODO this is not how msp.Error identifiers are supposed to be. Suggestion: ESLAVESPAWN or similar. This identifier needs defined as a constant in the msp dataobjects file.
 				Description:     fmt.Sprintf("Unable to start a mongod instance on port %d", m.Port),
-				LongDescription: fmt.Sprintf("ProcessManager.spawnProcess() failed for mongod on port %d with name %s\n%s", m.Port, m.ReplicaSetName, err.Error()),
+				LongDescription: fmt.Sprintf("ProcessManager.spawnProcess() failed for mongod on port %d with name %s\n%s", m.Port, m.ReplicaSetName, err.Error()), // TODO no newlines in errors?
 			}
 		}
 	} else {
@@ -69,13 +70,14 @@ func (c *Controller) EstablishMongodState(m msp.Mongod) *msp.Error {
 		go func() {
 			time.Sleep(MongodHardShutdownTimeout * time.Second)
 			c.processes.KillProcess(m.Port)
-			c.busyTable[m.Port].Unlock()
+			c.busyTable[m.Port].Unlock() // TODO document this line together with the Unlock() in m.State != msp.MongodStateDestroyed
 		}()
 	}
 
 	err := c.configurator.ApplyMongodConfiguration(m)
 
 	// do wait until the old instance is destroyed. Having a half destroyed unlocked instance flying around should be dangerous
+	// TODO better refer to the hard shutdown timeout above. AND: restructure this code because this is really the else part of the `if` above.
 	if m.State != msp.MongodStateDestroyed {
 		c.busyTable[m.Port].Unlock()
 	}
