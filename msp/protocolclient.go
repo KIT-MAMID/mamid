655dcaa223bc9414dd89b120f1ab96a5f6620253
Christian Schwarz <me@cschwarz.com>
2016-08-29 16:24:51 +0200

    ADD: msp: protoclient method for rsInitiate endpoint.

--- msp/protocolclient.go
+++ msp/protocolclient.go
@@ -12,6 +12,7 @@ var mspLog = logrus.WithField("module", "msp")
 
 type MSPClient interface {
 	RequestStatus(Target HostPort) ([]Mongod, *Error)
+	InitiateReplicaSet(Target HostPort, msg RsInitiateMessage) *Error
 	EstablishMongodState(Target HostPort, m Mongod) *Error
 }
 
@@ -99,4 +100,38 @@ func (c MSPClientImpl) EstablishMongodState(target HostPort, m Mongod) *Error {
 
 func constructBaseUrl(target HostPort) (base string) {
 	return fmt.Sprintf("https://%s:%d/", target.Hostname, target.Port)
+}
+
+func (c MSPClientImpl) InitiateReplicaSet(target HostPort, msg RsInitiateMessage) *Error {
+
+	buffer := new(bytes.Buffer)
+	err := json.NewEncoder(buffer).Encode(msg)
+	if err != nil {
+		mspLog.Errorf("msp: error serialzing RsInitiateMessage: %s", err)
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s:%d/msp/rsInitiate", target.Hostname, target.Port), buffer)
+	if err != nil {
+		mspLog.Errorf("msp: error creating request object for rsInitiate: %s", err)
+		panic(err)
+	}
+
+	resp, err := c.HttpClient.Do(req)
+
+	if err == nil {
+		if resp.StatusCode == http.StatusOK {
+			return nil
+		} else {
+			var slaveError Error
+			decodeErr := json.NewDecoder(resp.Body).Decode(&slaveError)
+			if decodeErr != nil {
+				return communicationErrorFromError(decodeErr)
+			}
+			return &slaveError
+		}
+	} else {
+		return communicationErrorFromError(err)
+	}
+
 }
