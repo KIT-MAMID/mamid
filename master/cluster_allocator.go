5ed3b75e67597130165ebf28ae7205356e8594d8
Anton Schirg <anton.schirg@posteo.de>
2016-09-04 03:55:56 +0200

    FIX: cluster_allocator: priority generation
    order by slave state corrected

--- master/cluster_allocator.go
+++ master/cluster_allocator.go
@@ -625,7 +625,7 @@ func DesiredMSPReplicaSetMembersForReplicaSetID(tx *gorm.DB, replicaSetID int64)
 		WHERE r.id = ?
 		      AND desired_state.execution_state = ?
 		ORDER BY
-			s.configured_state DESC, -- ordered by slave configured_state so that mongods on running slaves become voting first
+			s.configured_state ASC, -- ordered by slave configured_state so that mongods on running slaves become voting first
 			m.id ASC
 		`, SlaveStateDisabled, ReplicaSetMemberPriorityToBeRemoved, ReplicaSetMemberPriorityVolatile, ReplicaSetMemberPriorityPersistent, replicaSetID, MongodExecutionStateRunning,
 	).Rows()
